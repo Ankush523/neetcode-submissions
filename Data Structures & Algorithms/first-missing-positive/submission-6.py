755a8f8580f7e32849d4ccde0df8ff87dc277898
Ankush Dutta <ankush.ad050203@gmail.com>
2026-04-28 22:06:53 +0530

    Add: first-missing-positive - submission-6

---
+++ Data Structures & Algorithms/first-missing-positive/submission-6.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        hash_table = Counter(nums)
+        missing_vals = []
+        max_val = max(hash_table)
+        if (max_val < 0):
+            return 1
+        else:
+            for i in range(1, max_val+2):
+                if i not in hash_table:
+                    return i      
